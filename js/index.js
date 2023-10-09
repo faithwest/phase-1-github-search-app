182f4452c819891b6810c3203a864489c2477508
faithwest <faithchepkoech425@gmail.com>
2023-10-09 16:40:13 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -1,149 +1,99 @@
-// Define variables to store elements
-const searchForm = document.getElementById('search-form'); // Get the search form element
-const searchInput = document.getElementById('search-input'); // Get the search input element
-const resultsContainer = document.getElementById('results-container'); // Get the container for displaying search results
-const API_BASE_URL = 'https://api.github.com'; // Define the base URL for GitHub API
+// Step 1: Form Submission
+const searchForm = document.getElementById('searchForm');
+const searchInput = document.getElementById('searchInput');
+const userList = document.getElementById('userList');
+const repoList = document.getElementById('repoList');
 
-// Attach an event listener to the search form for handling user submission
-searchForm.addEventListener('submit', async (e) => {
-  e.preventDefault(); // Prevent the default form submission behavior
-  const searchTerm = searchInput.value.trim(); // Get the user's search term from the input field
+// Listen for form submission
+searchForm.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = searchInput.value.trim();
 
-  if (searchTerm === '') {
-    // If the search term is empty, display an error message
-    resultsContainer.innerHTML = '<p>Please enter a search term.</p>';
-  } else {
-    try {
-      // Create a URL for the user search endpoint with the search term
-      const userSearchURL = `${API_BASE_URL}/search/users?q=${searchTerm}`;
-      const response = await fetch(userSearchURL, {
-        headers: {
-          Accept: 'application/vnd.github.v3+json', // Specify the GitHub API version
-        },
+    if (searchTerm === '') {
+        alert('Please enter a username.');
+        return;
+    }
+
+    // Step 3: Call a function to search for users and display them
+    await searchUsers(searchTerm);
+});
+// Step 3: Search Users and Display
+async function searchUsers(username) {
+  try {
+      // Make a GET request to GitHub API with the username
+      const response = await fetch(`https://api.github.com/search/users?q=${username}`, {
+          headers: {
+              'Accept': 'application/vnd.github.v3+json'
+          }
       });
 
       if (!response.ok) {
-        // If the response status is not okay, display an error message
-        resultsContainer.innerHTML = `<p>Error: ${response.statusText}</p>`;
-        return;
+          throw new Error('GitHub API request failed');
       }
 
-      const data = await response.json(); // Parse the response data as JSON
-      const users = data.items; // Get the array of user objects from the response
+      const data = await response.json();
 
-      if (users.length === 0) {
-        // If no users were found, display a message
-        resultsContainer.innerHTML = '<p>No users found.</p>';
-      } else {
-        // If users were found, create HTML for displaying user information
-        const userListHTML = users
-          .map((user) => {
-            return `
-              <div class="user">
-                <img src="${user.avatar_url}" alt="${user.login}" />
-                <p><a href="${user.html_url}" target="_blank">${user.login}</a></p>
-              </div>
-            `;
-          })
-          .join(''); // Join the HTML for each user into a single string
+      // Clear previous user list
+      userList.innerHTML = '';
 
-        resultsContainer.innerHTML = userListHTML; // Display the user list on the page
-      }
-    } catch (error) {
-      // Handle any errors that occurred during the fetch or processing of data
-      resultsContainer.innerHTML = `<p>Error: ${error.message}</p>`;
-    }
+      // Loop through the user data and display user cards
+      data.items.forEach(user => {
+          const userCard = document.createElement('div');
+          userCard.classList.add('user-card');
+
+          // Display user information: username, avatar, and link to profile
+          userCard.innerHTML = `
+              <img src="${user.avatar_url}" alt="${user.login}">
+              <h3>${user.login}</h3>
+              <a href="${user.html_url}" target="_blank">View Profile</a>
+          `;
+
+          userList.appendChild(userCard);
+
+          // Step 4: Add a click event listener to each user card
+          userCard.addEventListener('click', () => {
+              // Step 4: Call a function to fetch and display user repositories
+              getUserRepos(user.login);
+          });
+      });
+  } catch (error) {
+      console.error(error);
   }
-});
-
-//display users// Attach a click event listener to the results container to handle user clicks on user items
-resultsContainer.addEventListener('click', async (e) => {
-    if (e.target.tagName === 'A') {
-      // Check if the clicked element is an anchor (link)
-      e.preventDefault(); // Prevent the default link behavior
-      const userURL = e.target.href; // Get the URL of the user's profile page
-  
-      try {
-        // Create a URL for the user's repositories endpoint
-        const userReposURL = `${userURL}/repos`;
-        const response = await fetch(userReposURL, {
-          headers: {
-            Accept: 'application/vnd.github.v3+json', // Specify the GitHub API version
-          },
-        });
-  
-        if (!response.ok) {
-          // If the response status is not okay, display an error message
-          resultsContainer.innerHTML = `<p>Error: ${response.statusText}</p>`;
-          return;
-        }
-  
-        const repos = await response.json(); // Parse the response data as JSON
-  
-        if (repos.length === 0) {
-          // If no repositories were found, display a message
-          resultsContainer.innerHTML = '<p>No repositories found for this user.</p>';
-        } else {
-          // If repositories were found, create HTML for displaying repository information
-          const repoListHTML = repos
-            .map((repo) => {
-              return `
-                <div class="repo">
-                  <p><a href="${repo.html_url}" target="_blank">${repo.name}</a></p>
-                </div>
-              `;
-            })
-            .join(''); // Join the HTML for each repository into a single string
-  
-          resultsContainer.innerHTML = repoListHTML; // Display the repository list on the page
-        }
-      } catch (error) {
-        // Handle any errors that occurred during the fetch or processing of data
-        resultsContainer.innerHTML = `<p>Error: ${error.message}</p>`;
-      }
-    }
-  });
-
-  //fetching and displaying 
-  function fetchUserRepositories(username) {
-    // Use fetch to make a GET request to the User Repos Endpoint
-    fetch(`https://api.github.com/users/${username}/repos`, {
-        headers: {
-            'Accept': 'application/vnd.github.v3+json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        displayUserRepositories(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
 }
+// Step 4: Fetch and Display User Repositories
+async function getUserRepos(username) {
+  try {
+      // Make a GET request to fetch user repositories
+      const response = await fetch(`https://api.github.com/users/${username}/repos`, {
+          headers: {
+              'Accept': 'application/vnd.github.v3+json'
+          }
+      });
 
-function displayUserRepositories(repositories) {
-    userRepos.innerHTML = ''; // Clear previous results
+      if (!response.ok) {
+          throw new Error('GitHub API request failed');
+      }
 
-    if (repositories.length === 0) {
-        userRepos.innerHTML = '<p>No repositories found for this user.</p>';
-        return;
-    }
+      const data = await response.json();
 
-    repositories.forEach(repo => {
-        const repoCard = document.createElement('div');
-        repoCard.classList.add('repo-card');
+      // Clear previous repo list
+      repoList.innerHTML = '';
 
-        const repoName = document.createElement('p');
-        repoName.textContent = repo.name;
+      // Loop through the user's repositories and display them
+      data.forEach(repo => {
+          const repoItem = document.createElement('div');
+          repoItem.classList.add('repo-item');
 
-        const repoLink = document.createElement('a');
-        repoLink.href = repo.html_url;
-        repoLink.textContent = 'View Repo';
-        repoLink.target = '_blank';
+          // Display repository information: name, description, and link
+          repoItem.innerHTML = `
+              <h3>${repo.name}</h3>
+              <p>${repo.description || 'No description available'}</p>
+              <a href="${repo.html_url}" target="_blank">Visit Repo</a>
+          `;
 
-        repoCard.appendChild(repoName);
-        repoCard.appendChild(repoLink);
-
-        userRepos.appendChild(repoCard);
-    });
+          repoList.appendChild(repoItem);
+      });
+  } catch (error) {
+      console.error(error);
+  }
 }
